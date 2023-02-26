b5297d52796228de5287ced55a60e7585ed6504f
Andrey454545 <109917137+Andrey454545@users.noreply.github.com>
2023-02-26 21:43:43 +0200

    update index.html

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@
 import { throttle } from 'lodash';
 
 // пошук елементів на сторінці
-const form = document.querySelector('feedback-form');
+const form = document.querySelector('.feedback-form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const LOCALSTORAGE_KEY = 'feedback-form-state'; // ключ для сховища
